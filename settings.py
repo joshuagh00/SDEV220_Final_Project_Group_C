e92fb8313debebb8d7f534c5544c0e3e3036ec2a
mdatkins <35694861+mdatkins@users.noreply.github.com>
2023-12-03 10:17:26 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,18 @@
+# settings.py
+# item descriptions, etc for inventory
+
+descriptions = [
+    ["GT4 turbine blade", r".\images\turbine_blade.png"],
+    ["FK2 fastener kit", r".\images\fasteners.png"],
+    ["R3 rod", r".\images\R3rod.png"],
+    ["T24 Tire ", r".\images\T24tire.png"],
+    ["T8 Tire ", r".\images\T8tire.png"],
+    ["W7 wheel", r".\images\W7wheel.png"],
+    ["S5 strut", r".\images\S5strut.png"],
+    ["A3 aileron", r".\images\A3aileron.png"],
+    ["P3 prop", r".\images\P3prop.png"],
+    ["P4 prop", r".\images\P4prop.png"],
+    ["P5 prop", r".\images\P5prop.png"],
+    ["R1 rudder", r".\images\R1rudder.png"],
+    ["F2 flap", r".\images\F2flap.png"]
+]
--- tkinter3.py
+++ tkinter3.py
@@ -30,27 +30,22 @@ Image of the item, if a single item is selected in the item list or search resul
 import tkinter as tk   ## tkinter GUI package
 from tkinter import messagebox
 from tkinter import * 
-import random
-import datetime
+
+import datetime  # for history list
 from PIL import ImageTk, Image  # to display current part image
+from settings import descriptions  # settings.py file contains item descriptions and imagefile names
+iLengthMax = 13  ## currently only 13 descriptions and images in settings.py
 
 import json  ## for writing inventory to disk file
 import os.path  ## to check for file existance
 
-random.seed()
-iLengthMax = 15
 filepath = './inventory.json'
-
-#img = []
-#for i in range(1)
-#    img[i] = Image.open('turbine_blade.png')
 
 IDmax = 9999
 
-
 class Item:
-    def __init__(self, ID=random.randrange(IDmax), description="TBD", quantity="1", imagefile = "nothing.png"):
-        self.ID = ID
+    def __init__(self, ID=IDmax, description="TBD", quantity="1", imagefile = "nothing.png"):
+        self.ID = int(ID)
         self.description = description
         self.quantity = quantity
         self.imagefile = imagefile  ## the image filedescription
@@ -62,17 +57,17 @@ class Inventory:
     def __init__(self):
         self.Items = {}
 
-    def add_Item(self, ID, description, qty, imagefile):
+    def add_Item(self, ID, description, qty,imagefile):
         if ID in self.Items:
             self.Items[ID].quantity += qty
         else:
-            self.Items[ID] = Item(ID, description, qty, imagefile)
+            self.Items[ID] = Item(ID, descriptions[int(ID)][0], qty, descriptions[int(ID)][1])
 
     def move_Item(self, ID, qty):
         if ID in self.Items and self.Items[ID].quantity >= qty:
             self.Items[ID].quantity -= qty  ## Fixme: move should append to history, not just subtract qty
         else:
-            messagebox.showerror("Error", "Not enough quantity in stock(%s) to move %s items"  % (self.Items[ID].quantity, qty))
+            messagebox.showerror("Error", "Not enough quantity in stock to move %s items"  % (qty))
  
     def search_Item(self, ID):
         if ID in self.Items and self.Items[ID].quantity >= 1:
@@ -100,44 +95,54 @@ class GUI(tk.Tk):
         self.inventory = inventory
         self.title("Inventory Management System")
 
-        self.Item_ID_label = tk.Label(self, text="Item ID")
-        self.Item_ID_label.place(x=50,y=600)
-        self.Item_ID_label.pack(side = LEFT)
-        self.Item_ID_entry = tk.Entry(self)
-        self.Item_ID_entry.pack(side = LEFT)
-        self.Item_ID_entry.insert(0, "0")
+        self.ID_label = tk.Label(self, text="Item ID")
+        self.ID_label.place(x=50,y=600)
+        self.ID_label.pack(side = LEFT)
+        self.ID_entry = tk.Entry(self)
+        self.ID_entry.pack(side = LEFT)
+        self.ID_entry.insert(0, "0")
+        self.ID_entry.bind('<Key>', self.update_image())  # update image when key pressed in this entry box
+        
+        self.descrip_label = tk.Label(self, text="  Description")
+        self.descrip_label.pack(side = LEFT)
+        self.descrip_entry = tk.Entry(self)
+        self.descrip_entry.pack(side = LEFT)
+        self.descrip_entry.insert(0, " description")
 
-        self.Item_description_label = tk.Label(self, text=" Description")
-        self.Item_description_label.pack(side = LEFT)
-        self.Item_description_entry = tk.Entry(self)
-        self.Item_description_entry.pack(side = LEFT)
-        self.Item_description_entry.insert(0, " description")
+        self.quantity_label = tk.Label(self, text="  Quantity")
+        self.quantity_label.pack(side = LEFT)
+        self.quantity_entry = tk.Entry(self)
+        self.quantity_entry.pack(side = LEFT)
+        self.quantity_entry.insert(0, " 1")
 
-        self.Item_quantity_label = tk.Label(self, text="Quantity")
-        self.Item_quantity_label.pack(side = LEFT)
-        self.Item_quantity_entry = tk.Entry(self)
-        self.Item_quantity_entry.pack(side = LEFT)
-        self.Item_quantity_entry.insert(0, " 1")
-
-        self.add_button = tk.Button(self, text="Add Item", command=self.add_Item)
+        self.add_button = tk.Button(self, text="New Item", command=self.add_Item)
         self.add_button.place(x=50,y=500)
 
         self.move_button = tk.Button(self, text="Move Item", command=self.move_Item)
         self.move_button.place(x=150,y=500)
 
-        self.search_button = tk.Button(self, text="Search", command=self.search_Item(self.Item_ID_entry))
+        self.search_button = tk.Button(self, text="Search", command=self.search_Item(self.ID_entry))
         self.search_button.place(x=250,y=500)
 
+    def update_image(self):
+        self.curr_ID = int(self.ID_entry.get()) ## 0  ## Kludge, fixme
+        self.img = Image.open(inventory.Items[self.curr_ID].imagefile)
+        self.tk_img = ImageTk.PhotoImage(self.img)
+        self.image_label = tk.Label(self, image=self.tk_img)
+        self.image_label.place(x=550,y=30)
+ 
+    
+
     def add_Item(self):
-        ID = self.Item_ID_entry.get()
-        description = self.Item_description_entry.get()
-        quantity = int(self.Item_quantity_entry.get())
-        imagefile = "nothing.png"
-        self.inventory.add_Item(ID, description, quantity, imagefile)
+        ID = int(self.ID_entry.get())
+        description = self.descrip_entry.get()
+        quantity = int(self.quantity_entry.get())
+        imagefile = descriptions[ID][1]
+        self.inventory.add_Item(ID, descriptions[ID][0], quantity, descriptions[ID][1])
 
     def move_Item(self):
-        ID = self.Item_ID_entry.get()
-        quantity = int(self.Item_quantity_entry.get())
+        ID = self.ID_entry.get()
+        quantity = int(self.quantity_entry.get())
         self.inventory.move_Item(ID, quantity)
 
     def search_Item(self, ID):
@@ -150,7 +155,7 @@ if __name__ == "__main__":
         inventory.load(filepath)
     else:    # Populate bogus sample inventory
         for i in range(iLengthMax):
-            inventory.add_Item(i, "Part type %s" % i, i+1,"turbine_blade.png")
+            inventory.add_Item(i, descriptions[i][0], i+1, descriptions[i][1])
  #      inventory.save(filepath)
 
     app = GUI(inventory)
@@ -168,11 +173,7 @@ if __name__ == "__main__":
  #       for i in inventory.Items:
  #           InventList.insert(END, inventory.Items[i].str())
         # display part image
-    curr_ID = int(app.Item_ID_entry.get()) ## 0  ## Kludge, fixme
-    img = Image.open(inventory.Items[curr_ID].imagefile)
-    tk_img = ImageTk.PhotoImage(img)
-    image_label = tk.Label(app, image=tk_img)
-    image_label.place(x=550,y=30)
+    app.update_image()
     app.mainloop()
  #  app.update_idletasks()
  #  app.update()
