ba8614750fb5230b2dd63c7854b82637780913f6
mdatkins <35694861+mdatkins@users.noreply.github.com>
2023-12-07 13:49:21 -0500

    Update gui.py
    Made entry a list so we can access elements elsewhere
    Removed message boxes from checkout_part, instead using the data from the entry boxes.
    
    Clarified titles of columns and entry boxes

--- gui.py
+++ gui.py
@@ -1,3 +1,7 @@
+gui.py  
+# 12/7/23 D. Kolb
+# 12/2/23 updated M. Atkins
+
 import tkinter as tk
 from tkinter import simpledialog
 from tracker import AircraftPartsTracker
@@ -6,17 +10,19 @@ class AircraftPartsGUI:
     def __init__(self, master, tracker):
         self.master = master
         self.master.title("Aircraft Parts Tracker")
-        self.master.geometry("800x600")
+        self.master.geometry("1000x1000")
 
         self.tracker = tracker
 
-        labels = ["Part Number", "Serial Number", "Description", "Condition", "Quantity"]
+        labels = ["Part Model #", "Serial #", "Description", "Condition", "Quantity"]
+        self.entry = [tk.Entry(master)] # length 1 list of entry boxes
+
         for row, label_text in enumerate(labels):
             label = tk.Label(master, text=label_text)
             label.grid(row=row, column=0, padx=10, pady=5, sticky="e")
 
-            entry = tk.Entry(master)
-            entry.grid(row=row, column=1, padx=10, pady=5, sticky="w")
+            self.entry.append (tk.Entry(master))
+            self.entry[row].grid(row=row, column=1, padx=10, pady=5, sticky="w")
 
         # Buttons
         receive_button = tk.Button(master, text="Receive Part", command=self.receive_part)
@@ -24,15 +30,6 @@ class AircraftPartsGUI:
 
         checkout_button = tk.Button(master, text="Checkout Part", command=self.checkout_part)
         checkout_button.grid(row=len(labels) + 1, column=0, columnspan=2, pady=10)
-
-        # Listbox for displaying inventory
-        self.inventory_listbox = tk.Listbox(master, selectmode=tk.SINGLE, height=10)
-        self.inventory_listbox.grid(row=len(labels) + 2, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
-
-        # Scrollbar for the listbox
-        scrollbar = tk.Scrollbar(master, command=self.inventory_listbox.yview)
-        scrollbar.grid(row=len(labels) + 2, column=2, sticky="nsew")
-        self.inventory_listbox.config(yscrollcommand=scrollbar.set)
 
         # Update inventory display initially
         self.tracker.update_inventory_display()
@@ -44,18 +41,20 @@ class AircraftPartsGUI:
         master.grid_columnconfigure(0, weight=1)
         master.grid_columnconfigure(1, weight=1)
 
+        self.part_info = [entry.get() for entry in self.master.winfo_children() if isinstance(entry, tk.Entry)]
+
+
     def receive_part(self):
-        part_info = [entry.get() for entry in self.master.winfo_children() if isinstance(entry, tk.Entry)]
-        self.tracker.receive_part(part_info)
+        self.tracker.receive_part(self.part_info)
         self.tracker.update_inventory_display()
 
     def checkout_part(self):
-        part_number = simpledialog.askstring("Checkout Part", "Enter Part Number:")
-        if part_number:
-            quantity = simpledialog.askinteger("Checkout Part", "Enter Quantity:")
-            if quantity:
-                self.tracker.checkout_part(part_number, quantity)
-                self.tracker.update_inventory_display()
+        #part_number = simpledialog.askstring("Checkout Part", "Enter Part Number:")
+        #if part_number:
+        #    quantity = simpledialog.askinteger("Checkout Part", "Enter Quantity:")
+        #    if quantity:
+        self.tracker.checkout_part(self.part_info[0], self.part_info[4])
+        self.tracker.update_inventory_display()
 
 
 if __name__ == "__main__":
